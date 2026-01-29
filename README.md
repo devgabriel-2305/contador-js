73888d23318a3752c72b8a32c3281fa8980b5519
Gabriel Damião <gabrieldamiao338@gmail.com>
2026-01-28 22:08:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# contador
+Contador interativo feito com HTML,  CSS e JavaScript. Permite contar entre dois valores com passo personalizado, exibindo o resultado dinamicamente.
---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById('btn')
+let res = document.getElementById('res')
+function calcular(){
+    let ini = document.getElementById('iinicio')
+    let fim = document.getElementById('ifim')
+    let pas = document.getElementById('ipasso')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        res.innerHTML = '[ERRO] Faltam dados!'
+    }else{
+        if (pas.value <= 0){
+            res.innerHTML = 'Passo inválido.'
+        }
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+                
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+    }
+    ini.value = ''
+    fim.value = ''
+    pas.value = ''
+}
+btn.addEventListener('click', calcular)
