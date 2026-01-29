3231f5ee3dafb589b2dd884200221be53f020198
Gabriel Damião <gabrieldamiao338@gmail.com>
2026-01-29 08:29:06 -0300

    JS

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ function calcular(){
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
         res.innerHTML = '[ERRO] Faltam dados!'
     }else{
-        if (pas.value <= 0){
-            res.innerHTML = 'Passo inválido.'
-        }
         res.innerHTML = 'Contando:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
+
+        if (pas.value <= 0){
+            res.innerHTML = 'Passo inválido.'
+        }
+        
         if (i < f){
             for(let c = i; c <= f; c += p){
                 res.innerHTML += ` ${c} \u{1F449}`
